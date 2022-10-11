dbb24a3db788ec196a29719473e6051e0de7b5e0
Adam Djo <adambenmessaoud2@gmail.com>
2022-10-11 23:57:28 +0100

    first commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import ContactForm from "../components/ContactForm";
 import Logo from "../components/Logo";
 import Mouse from "../components/Mouse";
 import Navigation from "../components/Navigation";
-import CopyToClipboard from "react-copy-to-clipboard";
+
 import SocialNetwork from "../components/SocialNetwork";
 import Buttons from "../components/Buttons";
 import { motion } from "framer-motion";
@@ -50,33 +50,28 @@ const Contact = () => {
           <div className="phone">
             <div className="content">
               <h4>Téléphone</h4>
-              <CopyToClipboard text="25555555" className="hover">
-                <p
-                  style={{ cursor: "pointer" }}
-                  className="clipboard"
-                  onClick={() => alert("Téléphone copié !")}
-                >
-                  25555555
-                </p>
-              </CopyToClipboard>
+
+              <p
+                style={{ cursor: "pointer" }}
+                className="clipboard"
+                onClick={() => alert("Téléphone copié !")}
+              >
+                25555555
+              </p>
             </div>
           </div>
 
           <div className="email">
             <div className="content">
               <h4>Email</h4>
-              <CopyToClipboard
-                text="adambenmessaoud2@gmail.com"
-                className="hover"
+
+              <p
+                style={{ cursor: "pointer" }}
+                className="clipboard"
+                onClick={() => alert("Email copié !")}
               >
-                <p
-                  style={{ cursor: "pointer" }}
-                  className="clipboard"
-                  onClick={() => alert("Email copié !")}
-                >
-                  adambenmessaoud2@gmail.com
-                </p>
-              </CopyToClipboard>
+                adambenmessaoud2@gmail.com
+              </p>
             </div>
           </div>
           <SocialNetwork></SocialNetwork>
